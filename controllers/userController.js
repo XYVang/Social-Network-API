5923226cfe8644984e7a2eebd972202dc452a3b9
Xavier <xaviervang1999@gmail.com>
2025-04-01 04:18:43 -0500

    Added controller code to interact with server

---
+++ controllers/userController.js
@@ -0,0 +1,49 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .populate('thoughts')
+      .populate('friends')
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getUserById(req, res) {
+    User.findById(req.params.userId)
+      .populate('thoughts')
+      .populate('friends')
+      .then((user) => (user ? res.json(user) : res.status(404).json({ message: 'No user found' })))
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findByIdAndUpdate(req.params.userId, req.body, { new: true })
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+    User.findByIdAndDelete(req.params.userId)
+      .then(() => res.json({ message: 'User deleted' }))
+      .catch((err) => res.status(500).json(err));
+  },
+  addFriend(req, res) {
+    User.findByIdAndUpdate(
+      req.params.userId,
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    ).then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  removeFriend(req, res) {
+    User.findByIdAndUpdate(
+      req.params.userId,
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    ).then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+};
