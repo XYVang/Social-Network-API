755e181973ec5ca9273bf0c4473a0b9280cd5aae
Xavier <xaviervang1999@gmail.com>
2025-04-01 04:25:59 -0500

    Fixed bug with using wrong controller code

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,16 +1,56 @@
-const router = require('express').Router();
-const {
-  getThoughts,
-  getThoughtById,
-  createThought,
-  updateThought,
-  deleteThought,
-  addReaction,
-  removeReaction,
-} = require('../controllers/thoughtController');
+const { Thought, User } = require('../models');
 
-router.route('/').get(getThoughts).post(createThought);
-router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
-router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
+module.exports = {
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  
+  getThoughtById(req, res) {
+    Thought.findById(req.params.thoughtId)
+      .then((thought) => thought ? res.json(thought) : res.status(404).json({ message: 'No thought found' }))
+      .catch((err) => res.status(500).json(err));
+  },
 
-module.exports = router;
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) =>
+        User.findByIdAndUpdate(req.body.userId, { $push: { thoughts: thought._id } }, { new: true })
+      )
+      .then(() => res.json({ message: 'Thought created successfully!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateThought(req, res) {
+    Thought.findByIdAndUpdate(req.params.thoughtId, req.body, { new: true })
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteThought(req, res) {
+    Thought.findByIdAndDelete(req.params.thoughtId)
+      .then(() => res.json({ message: 'Thought deleted' }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  addReaction(req, res) {
+    Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $push: { reactions: req.body } },
+      { new: true }
+    )
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  removeReaction(req, res) {
+    Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $pull: { reactions: { reactionId: req.body.reactionId } } },
+      { new: true }
+    )
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  }
+};
